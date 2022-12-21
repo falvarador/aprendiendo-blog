2fed7aa93438d6f7e68c3428fba8284618b2af13
falvarador <alvaguez1990@gmail.com>
2022-12-21 05:30:53 -0600

    Add custom font and inital styles

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,10 @@
+import rss from "@astrojs/rss";
+
+export const get = () =>
+  rss({
+    title: "Aprendiendo | Blog",
+    description: "Un viaje para compartir conocimiento y aprender juntos",
+    site: "https://aprendiendo.netlify.app",
+    items: import.meta.glob("./**/*.md"),
+    customData: `<language>es-mx</language>`,
+  });
